882107d260010bb8e4932b3b35ca8b6e4be20db6
Roshan <113022828+Roshan089@users.noreply.github.com>
2025-09-01 15:01:38 +0530

    Update priorty_queue.cpp

--- priorty_queue.cpp
+++ priorty_queue.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
     int arr[]={10,40,20,13,100};
     priority_queue<int> q;
+
+   // kth greatest
+    //    priority_queue<int, vector<int>, greater<int>> q;
+
     int n=5;
    
     int k;
@@ -17,9 +21,23 @@ int main(){
     }
     
     for(int i=k;i<n;i++){
-        if(arr[i]<q.top())
+        if(arr[i]<q.top()){
             q.pop();
             q.push(arr[i]);
+        }
+    }
+
+    //kth greatest
+       
+    for(int i=0;i<k;i++){
+        q.push(arr[i]);
+    }
+    
+    for(int i=k;i<n;i++){
+        if(arr[i]>q.top()){
+           q.pop();
+            q.push(arr[i]);
+        }
     }
     cout<<q.top();
 }
