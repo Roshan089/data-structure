b640c7c88db7abf8034625cb53627c4050e4ceec
Roshan <113022828+Roshan089@users.noreply.github.com>
2025-09-01 13:03:21 +0530

    Create heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    int arr[]={10,40,20,13,100};
+    priority_queue<int> q;
+    int n=5;
+   
+    int k;
+    cout<<"put k";
+    cin>>k;
+
+    
+    for(int i=0;i<k;i++){
+        q.push(arr[i]);
+    }
+    
+    for(int i=k;i<n;i++){
+        if(arr[i]<q.top())
+            q.push(arr[i]);
+    }
+    cout<<q.top();
+}
